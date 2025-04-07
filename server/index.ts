f4999c95f17ec9be422fb4a14e9476ca6805da56
Diggstown <diggstowndesigns@gmail.com>
2025-04-07 13:27:43 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import { storage } from "./storage";
 import { db } from "./db";
+console.log("🔥 App is starting up...");
 
 const app = express();
 app.use(express.json());
